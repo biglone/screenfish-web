1924ebc1dadcf73ba154ab8d30d445a8a898a126
Biglone <jjoyceu@gmail.com>
2026-01-08 10:53:07 +0800

    chore: bump build id to bust cache

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,8 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 
+(globalThis as Record<string, unknown>).__SCREENFISH_WEB_BUILD__ = '2026-01-08'
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
